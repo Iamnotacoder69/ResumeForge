e049e95c78e95b4c03f321381c5faa7dd70bd4ad
wassimmroueh999 <41749725-wassimmroueh999@users.noreply.replit.com>
2025-04-22 21:37:47 +0000

    Restored to '219735fe5033a08c57e40efe036fc7c5d72a09e9'
    Replit-Restored-To: 219735fe5033a08c57e40efe036fc7c5d72a09e9

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,15 @@
-import { Switch, Route, Redirect } from "wouter";
+import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
 import CVBuilder from "@/pages/CVBuilder";
-import LandingPage from "@/pages/LandingPage";
 
 function Router() {
   return (
     <Switch>
-      {/* Landing Page - Entry point */}
-      <Route path="/" component={LandingPage} />
-      
-      {/* CV Builder Routes */}
-      <Route path="/cv/template" component={CVBuilder} />
-      <Route path="/cv/edit/:id">
-        {(params) => <CVBuilder cvId={parseInt(params.id, 10)} />}
-      </Route>
-      
-      {/* Legacy route redirection */}
-      <Route path="/cv">
-        <Redirect to="/" />
-      </Route>
-      
-      {/* 404 Fallback */}
+      <Route path="/" component={CVBuilder} />
       <Route component={NotFound} />
     </Switch>
   );
